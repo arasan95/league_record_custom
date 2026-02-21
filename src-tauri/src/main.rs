7940625d62d4914b2909c7eb430b726a4d97bd94
arasan95 <arasan1115525@gmail.com>
2026-02-21 20:29:31 +0900

    Fix scoreboard display

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -42,7 +42,7 @@ fn main() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
         // tauri-plugin-log removed
-        // .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(WindowState::default())
         .manage(CurrentlyRecording::default())
         .manage(TrayState::default())
