c404285ec73e08d70d0843ce41a64d38d6af1ffe
arasan95 <arasan1115525@gmail.com>
2026-03-02 17:22:15 +0900

    chore: remove unused developer scripts and JSON dumps

--- dump_darius_w.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const d = JSON.parse(fs.readFileSync('tooltip_exports/ddragon_all_champions.json', 'utf8'));
-const s = d.Darius.spells.find(x => x.id === 'DariusNoxianTacticsONH');
-fs.writeFileSync('darius_w_cd.json', JSON.stringify({
-    vars: s.vars,
-    cd_dataValuesMap: s.cd_dataValuesMap,
-    cd_baseMap: s.cd_baseMap
-}, null, 2));
-console.log('Saved to darius_w_cd.json');
